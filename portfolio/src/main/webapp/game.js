6f3c0bf48ab86eeee847b02cafdc93e9ad085a57
Steph Walsh <srwalsh21@gmail.com>
2020-05-28 14:06:35 -0400

    Add board class to handle drawing and checking game state - will IIFE after game is playable

--- portfolio/src/main/webapp/game.js
+++ portfolio/src/main/webapp/game.js
@@ -1,57 +1,58 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-// is there some way to make this private?
-var board;
+class Board {
+  constructor() {
+    this.board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+    this.drawGrid();
+  }
 
-function init() {
-  board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
-  drawGrid(); 
-}
+  drawBall(xPos, yPos) {
+    ctx.beginPath();
+    ctx.arc(xPos, yPos, canvas.height / 8, 0, Math.PI*2, false);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+  }
 
-function drawBall(xPos, yPos) {
-  ctx.beginPath();
-  ctx.arc(xPos, yPos, canvas.height / 8, 0, Math.PI*2, false);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  ctx.closePath();
-}
+  drawGrid() {
+    // draws vertical lines in tic tac toe board
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 3, 0);
+    ctx.lineTo(canvas.width / 3, canvas.height);
+    ctx.stroke();
 
-function drawGrid() {
-  // draws vertical lines in tic tac toe board
-  ctx.beginPath();
-  ctx.moveTo(canvas.width / 3, 0);
-  ctx.lineTo(canvas.width / 3, canvas.height);
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(2 * canvas.width / 3, 0);
+    ctx.lineTo(2 * canvas.width / 3, canvas.height);
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.moveTo(2 * canvas.width / 3, 0);
-  ctx.lineTo(2 * canvas.width / 3, canvas.height);
-  ctx.stroke();
+    // draws horizontal lines in tic tac toe board
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 3);
+    ctx.lineTo(canvas.width, canvas.height / 3);
+    ctx.stroke();
 
-  // draws horizontal lines in tic tac toe board
-  ctx.beginPath();
-  ctx.moveTo(0, canvas.height / 3);
-  ctx.lineTo(canvas.width, canvas.height / 3);
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0, 2 * canvas.height / 3);
+    ctx.lineTo(canvas.width, 2 * canvas.height / 3);
+    ctx.stroke();
+  }
 
-  ctx.beginPath();
-  ctx.moveTo(0, 2 * canvas.height / 3);
-  ctx.lineTo(canvas.width, 2 * canvas.height / 3);
-  ctx.stroke();
-}
-
-function updateFrame(xPos, yPos) {
+  updateFrame(xPos, yPos) {
     // clear old frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall(xPos, yPos);
-    drawGrid();
+    this.drawBall(xPos, yPos);
+    this.drawGrid();
+  }
 }
 
+// create board object for game
+var gameBoard = new Board();
 
 /*
 * Updates the x and y vars to reflect curr mouse position on the canvas
@@ -60,7 +61,6 @@ canvas.addEventListener('mousedown', e => {
   var rect = canvas.getBoundingClientRect();
   var xPos = (e.clientX - rect.left) * (canvas.width / rect.width);
   var yPos = (e.clientY - rect.top) * (canvas.height / rect.height);
-  updateFrame(xPos, yPos);
+  gameBoard.updateFrame(xPos, yPos);
 });
 
-init();
