4ae59fd9bfa078a3a0b46483c1b79c0fb800d67d
Steph Walsh <srwalsh21@gmail.com>
2020-05-28 15:38:11 -0400

    Add correctly colored checkers at mouse location and ensure two checkers not in same spot

--- portfolio/src/main/webapp/game.js
+++ portfolio/src/main/webapp/game.js
@@ -9,12 +9,17 @@ class Board {
       [0, 0, 0]
     ];
     this.drawGrid();
+    var red = true; 
   }
 
-  drawBall(xPos, yPos) {
+  drawBall(xPos, yPos, redTrue) {
     ctx.beginPath();
     ctx.arc(xPos, yPos, canvas.height / 8, 0, Math.PI*2, false);
-    ctx.fillStyle = "red";
+    if (redTrue) {
+        ctx.fillStyle = "red";
+    } else {
+        ctx.fillStyle = "black";
+    }
     ctx.fill();
     ctx.closePath();
   }
@@ -46,8 +51,74 @@ class Board {
   updateFrame(xPos, yPos) {
     // clear old frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.drawBall(xPos, yPos);
+    this.determineSquare(xPos, yPos);
+    this.drawBoard();
     this.drawGrid();
+  }
+
+  determineSquare(xPos, yPos) {
+    var col = 0;
+    var row = 0; 
+
+    // finds col clicked
+    if (xPos < canvas.width / 3) {
+      col = 0;
+    } else if (xPos < 2 * canvas.width / 3) {
+      col = 1;
+    } else {
+      col = 2;
+    }
+
+    // finds row clicked
+    if (yPos < canvas.height / 3) {
+      row = 0;
+    } else if (yPos < 2 * canvas.height / 3) {
+      row = 1;
+    } else {
+      row = 2;
+    }
+
+    // can't place a tile over existing tile
+    if (this.board[row][col] == 0) {
+      if (this.redTrue) {
+        this.board[row][col] = 1;
+      } else {
+        this.board[row][col] = 2;
+      }
+      this.redTrue = !this.redTrue;
+    }
+  }
+
+  drawBoard() {
+    for (var i = 0; i < this.board.length; i++) {
+      for (var j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j] != 0) { 
+          var xMultiplier = 0;
+          var yMultiplier = 0;
+          if (i == 0) {
+            xMultiplier = 1;
+          } else if (i == 1) {
+            xMultiplier = 3;
+          } else {
+            xMultiplier = 5;
+          }
+          if (j == 0) {
+            yMultiplier = 1;
+          } else if (j == 1) {
+            yMultiplier = 3;
+          } else {
+            yMultiplier = 5;
+          }
+          
+          // draw checker
+          if (this.board[i][j] == 1) { 
+            this.drawBall(yMultiplier * canvas.width / 6, xMultiplier * canvas.height / 6, true);
+          } else {
+            this.drawBall(yMultiplier * canvas.width / 6, xMultiplier * canvas.height / 6, false);
+          }
+        }
+      }
+    }
   }
 }
 
