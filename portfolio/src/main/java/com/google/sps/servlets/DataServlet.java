dbb11d4b4c84d8fc1f3a86f6e711c2f5bb776611
Steph Walsh <srwalsh21@gmail.com>
2020-06-02 14:17:17 +0000

    Store the comment in Datastore, no fetching yet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -39,9 +42,17 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String comment = request.getParameter("text_input");
-    if (comment != null) {
-      comments.add(comment);
+    String name = request.getParameter("name_input");
+    if (comment == null || name == null) {
+      response.sendRedirect("/index.html");  
     }
+
+    Entity commentEntity = new Entity("Comment"); 
+    commentEntity.setProperty("title", comment);
+    commentEntity.setProperty("description", name);   
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity); 
     response.sendRedirect("/index.html");
   }
 }
