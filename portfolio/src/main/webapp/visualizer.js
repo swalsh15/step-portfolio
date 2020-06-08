ecdf69b52a3d5ba6ad9ff64f69e861ceab7c07e2
Steph Walsh <srwalsh21@gmail.com>
2020-06-08 14:53:47 +0000

    Add marker map from Google Chart API

---
+++ portfolio/src/main/webapp/visualizer.js
@@ -0,0 +1,23 @@
+google.charts.load('current', {
+  'packages':['geochart'],
+  'mapsApiKey': 'AIzaSyCsIDmQAVTMjvXr6NZBm1O8b9Ve9EWREwk'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+  const data = google.visualization.arrayToDataTable([
+    ['City',   'Population', 'Area'],
+    ['Chicago', 2000000, 1285.31],
+    ['New York City', 5000000, 1400.31],
+    ['Los Angeles', 4000000, 1300],
+    ['Philadelphia', 1500000, 800],
+  ]);
+
+  const options = {
+    region: 'US',
+    displayMode: 'markers',
+    colorAxis: {colors: ['blue', 'red']}
+  };
+  const chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+  chart.draw(data, options);
+}
