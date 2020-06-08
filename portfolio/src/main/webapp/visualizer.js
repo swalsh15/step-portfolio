2a51d36cba0be9af674ad8177749f2e7f0555fc7
Steph Walsh <srwalsh21@gmail.com>
2020-06-08 15:05:27 +0000

    Add API key for unlimited cities, define width in js

--- portfolio/src/main/webapp/visualizer.js
+++ portfolio/src/main/webapp/visualizer.js
@@ -11,12 +11,15 @@ function drawRegionsMap() {
     ['New York City', 5000000, 1400.31],
     ['Los Angeles', 4000000, 1300],
     ['Philadelphia', 1500000, 800],
+    ['Stamford', 30000, 900],
   ]);
 
   const options = {
     region: 'US',
     displayMode: 'markers',
-    colorAxis: {colors: ['blue', 'red']}
+    colorAxis: {colors: ['white', 'green']},
+    width: 900,
+    height: 500
   };
   const chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
   chart.draw(data, options);
