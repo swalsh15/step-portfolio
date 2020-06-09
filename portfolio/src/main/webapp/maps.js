40dde12b5e53acd31e577a21f1cee2e43a1710f2
Steph Walsh <srwalsh21@gmail.com>
2020-06-09 20:13:52 +0000

    Added maps with API and marked covid test sets in user's state

---
+++ portfolio/src/main/webapp/maps.js
@@ -0,0 +1,62 @@
+/** Creates a map and adds it to the page. Adds markers to maps with COVID 
+* test sites for CT. 
+*/
+function createMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 41.05, 
+      lng: -73.53
+    }, 
+    zoom: 8
+  });
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  let state = "connecticut";
+  // gets user's current location to center map and decide what state to load
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + ",";
+      url += position.coords.longitude + "&key=AIzaSyCsIDmQAVTMjvXr6NZBm1O8b9Ve9EWREwk";
+
+      // gets state from coordiates
+      fetch(url).then(response => response.json()).then(result => {
+        state = result.results[7].address_components[0].long_name;
+        state = state.toLowerCase();
+      }); 
+
+      initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(initialLocation);
+    });
+  }
+
+  // call API to return testing site locations for variable state 
+  fetch("https://covid-19-testing.github.io/locations/" + state + "/complete.json", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      for (let i = 0; i < result.length; i++) {
+        let street = result[i].physical_address[0].address_1;
+        street = street.replace(" ", "+");
+        let city = result[i].physical_address[0].city;
+        city = city.replace(" ", "+");
+        const state = result[i].physical_address[0].state_province;
+
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        url += street + ",+";
+        url += city + ",+";
+        url += state + "&key=AIzaSyCsIDmQAVTMjvXr6NZBm1O8b9Ve9EWREwk";
+      
+        // fetch coordinates from current address
+        fetch(url).then(response => response.json()).then((coords) => {
+          const latitude = coords.results[0].geometry.location.lat;
+          const longitude = coords.results[0].geometry.location.lng;
+          const markerLocation = {lat: latitude, lng: longitude};
+          const marker = new google.maps.Marker({position: markerLocation, map: map});
+        });
+
+      }  
+    })
+  .catch(error => console.log('error', error));
+}
